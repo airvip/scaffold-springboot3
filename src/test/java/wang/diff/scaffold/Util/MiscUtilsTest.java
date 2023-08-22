497404346446abf22b0cc3b5dd76633d767424ef
airvip <sdqhwzb@gmail.com>
2023-08-22 15:54:45 +0800

    fix:unit test mist

---
+++ src/test/java/wang/diff/scaffold/Util/MiscUtilsTest.java
@@ -0,0 +1,15 @@
+package wang.diff.scaffold.Util;
+
+import org.junit.jupiter.api.Test;
+
+public class MiscUtilsTest {
+
+    @Test
+    public void test() {
+        String upperCase = "37142520000101873x".toUpperCase();
+        System.out.println(upperCase);
+
+        upperCase = "37142520000101873X".toUpperCase();
+        System.out.println(upperCase);
+    }
+}
